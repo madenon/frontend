b3dd073e97aedeac2b6e95a5377d8934a7596262
madenon <brcgr230@gmail.com>
2024-08-04 19:22:49 +0100

    debut

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur vitae non omnis harum odio dolores suscipit nesciunt rem soluta quae? Reiciendis, quo. Quo voluptatum, temporibus numquam possimus ut error animi vero architecto quae ratione magnam necessitatibus, qui magni eius! Aperiam, eius obcaecati est mollitia molestiae, architecto labore autem quas amet illo modi natus non praesentium eum. Veritatis blanditiis placeat possimus ad facere. Eum repudiandae mollitia eaque nobis numquam aut perspiciatis excepturi fuga perferendis inventore officia, vitae consequatur ullam, sed temporibus eos, assumenda optio facere quibusdam molestias. Dolor vitae nulla in. Quos, dolore cumque provident sapiente ex ducimus ea soluta corporis ullam nostrum eius rem non natus maxime iste facilis reprehenderit commodi vitae molestias, adipisci aspernatur consectetur. Velit perspiciatis sapiente vitae quis blanditiis neque minima eaque rerum illo odio nemo explicabo cum quam fuga, est sequi temporibus? Obcaecati est, aspernatur repellat dolores qui velit adipisci fuga magni quaerat ex? Omnis dolore quidem vel placeat enim eius beatae quo porro commodi. Facilis cupiditate assumenda debitis voluptate molestias tenetur quo eveniet, culpa temporibus, et tempore facere consequuntur delectus nobis odio consequatur id. Quis ad ducimus fuga eaque placeat dolore explicabo cupiditate fugit, numquam consectetur incidunt animi, omnis nesciunt facilis quibusdam accusantium. In, amet?
+    </div>
+  )
+}
+
+export default App
